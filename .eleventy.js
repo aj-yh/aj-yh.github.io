21f82a860bfa02b7daae529fc2f34b314c0401e1
aj-yh <ajyhayashi@gmail.com>
2026-05-21 00:04:16 -0500

    Added resume and updated link spacing

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/assets");
   return {
     dir: { input: "src", output: "_site" },
     templateFormats: ["njk", "html"],
